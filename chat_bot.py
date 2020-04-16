5ad58326eeabe099efb4a8f0a291df4827094b44
Oscar Batori <oscar@liquidata.co>
2020-04-16 13:05:33 -0700

    command line arguments, and clean up

--- chat_bot.py
+++ chat_bot.py
@@ -3,18 +3,14 @@
 import random
 from doltpy.core import Dolt, clone_repo
 import os
+import argparse
 
-
-def censor_text(text, bad_words_df):
-    censored = False
-    censored_text = text
-    for bad_word in bad_words_df['bad_word']:
-        # If `bad_word` exists as a substring of `text`, replace the substring with asterisks
-        if text.find(bad_word) > -1:
-            censored_text = censored_text.replace(bad_word, '*'*len(bad_word))
-            censored = True
-    return censored_text, censored
-
+ON_LOAD_TEXT = '''
+Hello! This is a simple chat bot with profanity filter.
+Respond 'bye', or use CTRL+C to exit.
+You can type anything you want, and I will censor the bad words.
+Say something
+'''
 RESPONSES = ['Cool',
              'Sounds good',
              'Thanks',
@@ -33,35 +29,71 @@ RESPONSES = ['Cool',
              "Sure"]
 
 
-CHECKOUT_DIR = 'bad-words-clone'
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('remote_name', help='The DoltHub remote from which to pull a set of bad words')
+    parser.add_argument('checkout_dir', help='The local directory to clonet the remote too')
+    args = parser.parse_args()
 
-def clone_or_pull_latest():
-    if os.path.exists(CHECKOUT_DIR):
-        repo = Dolt(CHECKOUT_DIR)
+    # Read in bad words
+    repo = clone_or_pull_latest(args.remote_name, args.checkout_dir)
+    bad_words_df = repo.read_table('bad_words')
+
+    # Enter chat loop
+    chat_loop(bad_words_df)
+
+
+def chat_loop(bad_words_df):
+    """
+    Maintains a censored conversation with the user
+    :param bad_words_df:
+    :return:
+    """
+    print(ON_LOAD_TEXT)
+    keep_chatting = True
+    while keep_chatting:
+        user_response = input("> Me: ")
+        if user_response.lower() == "bye":
+            print("> ChatBot: Thanks for chatting!")
+            keep_chatting = False
+        else:
+            censored_text, censored = censor_text(user_response.lower(), bad_words_df)
+            if censored:
+                print("> ChatBot sees:", censored_text)
+            print("> ChatBot:", random.choice(RESPONSES))
+
+
+def clone_or_pull_latest(remote_name, checkout_dir):
+    """
+    Clones the remote to the speicified location if checkout_dir/.dolt does not exist, pulls the latest otherwise
+    :param remote_name:
+    :param checkout_dir:
+    :return:
+    """
+    if os.path.exists(checkout_dir):
+        repo = Dolt(checkout_dir)
         repo.pull()
         return repo
     else:
-        return clone_repo('Liquidata/bad-words', CHECKOUT_DIR)
+        return clone_repo(remote_name, checkout_dir)
 
-KEEP_CHATTING = True
 
-on_load_text = '''
-Hello! This is a simple chat bot with profanity filter.
-Respond 'bye', or use CTRL+C to exit.
-You can type anything you want, and I will censor the bad words.
-Say something
-'''
-print(on_load_text)
+def censor_text(text, bad_words_df):
+    """
+    Given the string text, uses the bad_words_df['bad_words'] to censor text, returns
+    :param text:
+    :param bad_words_df:
+    :return:
+    """
+    censored = False
+    censored_text = text
+    for bad_word in bad_words_df['bad_word']:
+        # If `bad_word` exists as a substring of `text`, replace the substring with asterisks
+        if text.find(bad_word) > -1:
+            censored_text = censored_text.replace(bad_word, '*'*len(bad_word))
+            censored = True
+    return censored_text, censored
 
-repo = clone_or_pull_latest()
-while KEEP_CHATTING:
-    bad_words_df = repo.read_table('bad_words')
-    user_response = input("> Me: ")
-    if user_response.lower() == "bye":
-        print("> ChatBot: Thanks for chatting!")
-        KEEP_CHATTING = False
-    else:
-        censored_text, censored = censor_text(user_response.lower(), bad_words_df)
-        if censored == True:
-           print("> ChatBot sees:", censored_text)
-        print("> ChatBot:",random.choice(RESPONSES))
+
+if __name__ == '__main__':
+    main()
